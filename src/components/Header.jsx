9102021a5ccbc356048472d39cceea0e92dc54ed
Manav Misra <manavm@visionwebsoft.com>
2022-11-27 13:01:44 -0600

    Use Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import reactLogo from "../assets/react.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    </header>
+  );
+}
