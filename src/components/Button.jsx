e0c21f3c2d4f8ab38b4fec08e22952ff0a98e926
Manav Misra <manavm@visionwebsoft.com>
2022-11-27 18:15:37 -0600

    Log dataset for count

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ import PropTypes from "prop-types";
 
 export default function Button(props) {
   return (
-    <button className="rounded bg-blue-500 px-4 py-2 text-white">
+    <button
+      className="rounded bg-blue-500 px-4 py-2 text-white"
+      data-count={props.text}
+      onClick={props.handleClick}
+    >
       Add {props.text}
     </button>
   );
@@ -10,4 +14,5 @@ export default function Button(props) {
 
 Button.propTypes = {
   text: PropTypes.number.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,15 @@ const BUTTONS = [1, 2, 3, 4, 5];
 export default function Main() {
   const [count, setCount] = useState(0);
 
+  const handleClick = (e) => {
+    console.log(e.target.dataset.count);
+  };
+
   return (
     <main className="flex flex-col items-center gap-y-4 ">
       <div className="flex gap-x-4">
         {BUTTONS.map((num) => (
-          <Button text={num} key={num} />
+          <Button key={num} text={num} handleClick={handleClick} />
         ))}
       </div>
       <p className="text-center text-5xl font-black">{count}</p>
