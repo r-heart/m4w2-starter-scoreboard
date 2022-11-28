9ac55352bd7f84a26a6d9b70f206bbfaa8ee18f8
Manav Misra <manavm@visionwebsoft.com>
2022-11-27 18:04:35 -0600

    Remove Footer component
    Prettifies code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Footer from "./components/Footer";
 import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center text-3xl font-extrabold my-4">Scoreboard App</h1>
+      <h1 className="my-4 text-center text-3xl font-extrabold">
+        Scoreboard App
+      </h1>
       <Main />
-      <Footer />
     </div>
   );
 }
