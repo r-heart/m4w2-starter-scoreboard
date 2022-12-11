7dae4e48a531539eb470a0cb72fe2e5e8d40e20a
Manav Misra <manavm@visionwebsoft.com>
2022-12-11 00:41:50 -0600

    feat: :sparkles:

--- src/components/HomeAwaySwitch.jsx
+++ src/components/HomeAwaySwitch.jsx
@@ -1,14 +1,26 @@
+import PropTypes from "prop-types";
+
 // https://dev.to/themesberg/building-a-tailwind-css-toggleswitch-component-4pc3
-export default function HomeAwaySwitch() {
+export default function HomeAwaySwitch({ handleToggle }) {
   return (
     <label
       htmlFor="switch"
       className="switch relative flex cursor-pointer items-center"
     >
       <span className="mr-1 text-sm">Home</span>
-      <input type="checkbox" id="switch" className="sr-only" />
+      <input
+        type="checkbox"
+        id="switch"
+        className="sr-only"
+        onChange={handleToggle}
+        defaultChecked
+      />
       <span className="h-6 w-12 rounded-full bg-gray-800" />
       <span className="ml-1 text-sm">Away</span>
     </label>
   );
 }
+
+HomeAwaySwitch.propTypes = {
+  handleToggle: PropTypes.func.isRequired,
+};
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import CONFIG from "../config";
 import Buttons from "./Buttons/Buttons";
 import Select from "./Select";
+import HomeAwaySwitch from "./HomeAwaySwitch";
 
 export default function Main() {
   const [buttons, setButtons] = useState([]);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [homeAway, setHomeAway] = useState("away");
 
   return (
     <main className="flex flex-col items-center gap-y-4 ">
@@ -19,7 +23,33 @@ export default function Main() {
         }}
       />
 
-      <Buttons buttons={buttons} handleClick={() => {}} />
+      <HomeAwaySwitch
+        handleToggle={() => {
+          setHomeAway((prev) => (prev === "home" ? "away" : "home"));
+        }}
+      />
+
+      <Buttons
+        buttons={buttons}
+        handleClick={(e) => {
+          if (homeAway === "home") {
+            setHomeScore((prev) => prev + Number(e.target.dataset.count));
+          } else {
+            setAwayScore((prev) => prev + Number(e.target.dataset.count));
+          }
+        }}
+      />
+
+      <section className="flex flex-row gap-x-4">
+        <div className="flex flex-col items-center">
+          <h2 className="text-2xl">Home</h2>
+          <p className="text-4xl">{homeScore}</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <h2 className="text-2xl">Away</h2>
+          <p className="text-4xl">{awayScore}</p>
+        </div>
+      </section>
     </main>
   );
 }
