fd734cfe7d39b5d64464cea17b1ad5dfe5e7823b
r-heart <rachaelkheart@gmail.com>
2023-07-10 17:05:42 -0700

    refactor: :recycle: replace extra <div> with a React Fragment component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
+    // uUing shorthand for a React Fragment
+    <>
       <h1 className="my-4 text-center text-3xl font-extrabold">
         Scoreboard App
       </h1>
       <Main />
-    </div>
+    </>
   );
 }
 
