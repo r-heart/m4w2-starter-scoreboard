619baec4451bed235ac147684dd6257853f296e2
r-heart <rachaelkheart@gmail.com>
2023-09-12 20:15:58 -0700

    test: :camera_flash: update test to add it shows minutes and seconds in proper format

--- src/tests/Scoreboard.test.jsx
+++ src/tests/Scoreboard.test.jsx
@@ -5,3 +5,14 @@ test("renders the scoreboard", () => {
   const rendered = render(<Scoreboard />);
   expect(rendered).toMatchSnapshot();
 });
+
+it("initial render", () => {
+  const rendered = render(<Scoreboard timeRemaining={1} />);
+  expect(rendered).toMatchSnapshot();
+});
+
+it("shows minutes and seconds in proper format", () => {
+  render(<Scoreboard timeRemaining={899} />);
+  const timeP = screen.getByTestID("time");
+  expect(timeP).toHaveTextContent("14:59");
+});
