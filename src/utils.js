90f2c8b8fadae6826848abd762324b813ecefa83
r-heart <rachaelkheart@gmail.com>
2023-09-20 16:47:04 -0700

    fix: :bug: fix utils.js to format minutes and seconds correctly and pass utils.test.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,9 @@
 export function formatMinutesAndSeconds(seconds) {
-  return `${Math.floor(seconds / 60)}:${String(seconds % 60)
-    .toString()
-    .padStart(2, "0")}`;
+  const totalMinutes = Math.floor(seconds / 60);
+  const minutes = totalMinutes < 1 ? `0${totalMinutes}` : `${totalMinutes}`;
+  const remainingSeconds = seconds % 60;
+  const formattedSeconds =
+    remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+
+  return `${minutes}:${formattedSeconds}`.toString();
 }
