94ba036a5101cc083bc32888667e257a1fe34f03
r-heart <rachaelkheart@gmail.com>
2023-09-20 16:57:17 -0700

    test: :white_check_mark: fix Display.test.jsx to pass the initial render, update snapshots for Display.test.jsx and Scorecoard.test.jsx so tests will be of the correct snapshots

--- src/tests/Display.test.jsx
+++ src/tests/Display.test.jsx
@@ -1,7 +1,7 @@
 import { act, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { vi } from "vitest";
-import Display from "../components/Display";
+import Display from "../routes/Display";
 
 test("initial render", () => {
   const rendered = render(
