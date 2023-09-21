79520117958aa38b5ce0ec8c9bf138e901e406e8
r-heart <rachaelkheart@gmail.com>
2023-09-20 17:48:19 -0700

    test: :white_check_mark: update ./src/setUpTests.js as directed

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,15 @@
 import "@testing-library/jest-dom";
-import { cleanup } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { afterEach } from "vitest";
 
 afterEach(() => {
   cleanup();
 });
+
+export function setup(component) {
+  return {
+    user: userEvent.setup(),
+    ...render(component),
+  };
+}
